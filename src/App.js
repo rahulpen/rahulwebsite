350930c11a811b47661e25ecb42afc7eb6ff76c4
Rahul <rahul.pen24@gmail.com>
2020-04-04 18:22:39 -0700

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Rahul Pendyala</h1>
+        <h4>Software Engineer</h4>
       </header>
+
+      <body className="App-body">
+        <p>Welcome to my personal website!</p>
+      </body>
+
+      <footer className="App-footer">
+        <p>Under Construction - Built in quarantine</p>
+      </footer>
     </div>
   );
 }
