d70bcf72eaad48a74c3a5d378665c48b955f81f4
Rahul Pendyala <rahulpendyala@Mac.hsd1.ca.comcast.net>
2026-09-12 19:54:12 -0700

    Fix footer icons layout to stay on a single horizontal row

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ export const App: React.FC = () => {
       <main className="App-body" />
 
       <footer className="App-footer">
-        <p>
+        <div className="social-links">
           <a
             className="social-link"
             href="mailto:rahul.pen24@gmail.com"
@@ -39,7 +39,6 @@ export const App: React.FC = () => {
 
           <a
             className="social-link"
-            style={{ paddingLeft: '1%' }}
             target="_blank"
             rel="noopener noreferrer"
             href="https://www.linkedin.com/in/rahul-pendyala-4a4060163/"
@@ -50,7 +49,6 @@ export const App: React.FC = () => {
 
           <a
             className="social-link"
-            style={{ paddingLeft: '1%' }}
             target="_blank"
             rel="noopener noreferrer"
             href="https://github.com/rahulpen"
@@ -72,8 +70,8 @@ export const App: React.FC = () => {
               <path d="M9 19c-4.286 1.35-4.286-2.55-6-3m12 5v-3.5c0-1 .099-1.405-.5-2 2.791-.3 5.5-1.366 5.5-6.04a4.567 4.567 0 0 0 -1.333 -3.21 4.192 4.192 0 00-.08-3.227s-1.05-.3-3.476 1.267a12.334 12.334 0 0 0 -6.222 0C6.462 2.723 5.413 3.023 5.413 3.023a4.192 4.192 0 0 0 -.08 3.227A4.566 4.566 0 004 9.486c0 4.64 2.709 5.68 5.5 6.014-.591.589-.56 1.183-.5 2V21" />
             </svg>
           </a>
-        </p>
-        <p>Under Construction</p>
+        </div>
+        <p className="status-text">Under Construction</p>
       </footer>
     </div>
   );
